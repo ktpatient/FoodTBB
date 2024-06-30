09b7e6b055a0df93633109b31ff28d8498087020
ktpatient <167013520+ktpatient@users.noreply.github.com>
2024-06-30 13:11:05 +0100

    Add more cake flavours

--- src/main/java/com/kitp13/food/registry/FoodRegistry.java
+++ src/main/java/com/kitp13/food/registry/FoodRegistry.java
@@ -8,9 +8,12 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class FoodRegistry {
-    public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FoodMod.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FoodMod.MODID);
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, FoodMod.MODID);
 
     public static CakeFlavour CAKE_FLAVOUR_CHOCOLATE = new CakeFlavour("chocolate");
-
+    public static CakeFlavour CAKE_FLAVOUR_STRAWBERRY = new CakeFlavour("strawberry");
+    public static CakeFlavour CAKE_FLAVOUR_MINT = new CakeFlavour("mint");
+    public static CakeFlavour CAKE_FLAVOUR_VANILLA = new CakeFlavour("vanilla");
+    public static CakeFlavour CAKE_FLAVOUR_PLAIN = new CakeFlavour("plain");
 }
